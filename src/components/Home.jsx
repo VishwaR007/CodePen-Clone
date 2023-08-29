6032d27cf1707fa929e2cc3b0e1d6481181b7e48
Vishwanath R <vishwar07770@gmail.com>
2023-08-29 19:55:57 +0530

    CodePen Clone

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+
+import Header from './Headder'
+import Code from './Code';
+import Result from './Result'
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Code />
+            <Result />
+        </>
+    )
+}
+
+export default Home;
